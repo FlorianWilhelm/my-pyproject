7681b7247c614520d5d17c801502eef9db2d0a1f
Florian Wilhelm <FlorianWilhelm@users.noreply.github.com>
2024-01-03 13:46:28 +0100

    Update my_module.py

--- src/my_pyproject/my_module.py
+++ src/my_pyproject/my_module.py
@@ -2,6 +2,6 @@ def fib(n: int) -> int:
     """Fibonacci example function"""
     assert n > 0
     a, b = 1, 1
-    for _i in range(n - 1):
+    for _ in range(n - 1):
         a, b = b, a + b
     return a
